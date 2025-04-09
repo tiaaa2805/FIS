496f60d7f1be4dddada4b08f0be25a584f8afd3e
Student <student@b426-14.intranet.cs.upt.ro>
2025-04-09 19:09:44 +0300

    second

---
+++ bin/hello-maven.cmd
@@ -0,0 +1,2 @@
+@echo off
+java -jar %USERPROFILE%\.m2\repository\org\example\hello-maven\0.1.0-SNAPSHOT\hello-maven-0.1.0-SNAPSHOT.jar %*
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnsiConsole.systemInstall();
+
+        String language;
+
+        if (args.length == 0) {
+            language = "en";
+        } else {
+            language = args[0].toLowerCase();
+        }
+        Greeting gp = new Greeting(language);
+
+        System.out.println(Ansi.ansi()
+                .fg(gp.getGreetingColor()).a(gp.getGreeting() + " ")
+                .fg(gp.getPersonColor()).a(gp.getPerson() + " ")
+                .fg(gp.getPunctuationColor()).a(gp.getPunctuation())
+                .reset()
+        );
+
+        AnsiConsole.systemUninstall();
     }
 }
+///trebuue mereu resincuit mergem in Maven din dreapta si dam resincuire
+///apoi mai tarziu la mvm clean package trebuie sa ma mut in folderul de baza!!!
+///ala cu exec trebuie pus in plugins
